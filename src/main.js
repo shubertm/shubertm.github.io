7c9aae8634f70f250d7f012f933a8607dbda3196
Shubert Munthali <shubertm.m@gmail.com>
2025-07-16 11:34:41 +0200

    Style projects

--- src/main.js
+++ src/main.js
@@ -1,5 +1,4 @@
 import './style.css'
-import { setupCounter } from './counter.js'
 
 document.querySelector('#app').innerHTML = `
   <div>
@@ -14,5 +13,3 @@ document.querySelector('#app').innerHTML = `
     </p>
   </div>
 `
-
-setupCounter(document.querySelector('#counter'))
