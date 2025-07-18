a888251fe53dbf645f00f910152fd8b422a4a4c5
Shubert Munthali <shubertm.m@gmail.com>
2025-07-18 15:17:58 +0200

    Add base to vite config

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,7 @@ import {defineConfig} from "vite";
 export default defineConfig(
     {
         plugins: [],
-        baseUrl: 'https://shubertm.github.io/',
+        base: "./",
+        baseUrl: 'https://shubertm.github.io',
     }
 )
