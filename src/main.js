65dfc2b611151abbf2d26b77c1bf9431232f81bc
Shubert Munthali <shubertm.m@gmail.com>
2026-01-07 20:31:13 +0200

    Minor fix

--- src/main.js
+++ src/main.js
@@ -16,7 +16,7 @@ const mePage = `
     </a>
     <h1>Shubert Munthali</h1>
     <h2>Hello!</h2>
-    <p>I am a <b>Software Engineer</b> from the other side of the earth, my interests in computing are <b>Secure P2P networks</b>, <b>Blockchain</b>, <b>Cryptography</b>, <b>Algorithms and Data Structures</b> and <b>Mobile Development</b>. I also take pleasure in exploring electronics and robotics, I mean... these sparking creations are awesome!</p>
+    <p>I am a <b>Software Engineer</b> from the other side of the earth, my interests in computing are <b>Secure P2P networks</b>, <b>Blockchain</b>, <b>Cryptography</b>, <b>Data Structures and Algorithms</b> and <b>Mobile Development</b>. I also take pleasure in exploring electronics and robotics, I mean... these sparking creations are awesome!</p>
     <p class="star-trek">
       "To boldly go where no one has gone before" - Star Trek
     </p>
